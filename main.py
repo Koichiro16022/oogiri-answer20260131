da0c2d87d4b069c1912b4a2f3c72b85832b6fc59
Koichiro16022 <ishida.koichiro16022@gmail.com>
2026-02-13 19:13:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -127,8 +127,8 @@ def build_controlled_audio(full_text, mode="gtts"):
     for i, part in enumerate(parts):
         if not part: continue
         if '_' in part:
-            # --- 修正：0.1 を 0.05 に変更 ---
-            duration = len(part) * 0.05
+            # --- 修正：0.1 を 0.1 に変更 ---
+            duration = len(part) * 0.1
             clips.append(make_silence(duration))
         else:
             tmp_filename = f"part_{mode}_{i}.mp3"
